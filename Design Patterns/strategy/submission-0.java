1e485d2a3048a29bbfcf8c1b0b076ed5a54efeb3
Surendar <79657418+surendar0101@users.noreply.github.com>
2026-04-06 21:25:41 +0530

    Add: strategy - submission-0

---
+++ Design Patterns/strategy/submission-0.java
@@ -0,0 +1,65 @@
+class Person {
+    private String lastName;
+    private int age;
+    private boolean isMarried;
+
+    public Person(String lastName, int age, boolean isMarried) {
+        this.lastName = lastName;
+        this.age = age;
+        this.isMarried = isMarried;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isMarried() {
+        return isMarried;
+    }
+}
+
+interface PersonFilter {
+    boolean apply(Person person);
+}
+
+class AdultFilter implements PersonFilter {
+
+    public boolean apply(Person person) {
+        return person.getAge() >= 18;
+    }
+}
+
+class SeniorFilter implements PersonFilter {
+    
+    public boolean apply(Person person) {
+        return person.getAge() >= 65;
+    }
+}
+
+class MarriedFilter implements PersonFilter {
+    
+    public boolean apply(Person person) {
+        return person.isMarried();
+    }
+}
+
+class PeopleCounter {
+    private PersonFilter filter;
+
+    public void setFilter(PersonFilter filter) {
+        this.filter = filter;
+    }
+
+    public int count(List<Person> peopleList) {
+        int result = 0;
+        for (Person people: peopleList) {
+            if (this.filter.apply(people))
+                result++;
+        }
+        return result;
+    }
+}
