a97e8213a9f8a30980b69547a091092e6c7f1738
Surendar <79657418+surendar0101@users.noreply.github.com>
2026-04-13 21:55:14 +0530

    Add: subsets - submission-13

---
+++ Data Structures & Algorithms/subsets/submission-13.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void dfs(int i, int[] nums, List<Integer> subset, List<List<Integer>> result ) {
+        if (i >= nums.length) {
+            result.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[i]);
+        dfs(i + 1, nums, subset, result);
+        subset.remove(subset.size() - 1);
+        dfs(i + 1, nums, subset, result);
+    }
+}
