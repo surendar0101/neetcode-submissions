782c4bc9971a4a484b74d84f8118565696920859
Surendar <79657418+surendar0101@users.noreply.github.com>
2026-05-08 22:31:38 +0530

    Add: merge-intervals - submission-5

---
+++ Data Structures & Algorithms/merge-intervals/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1)
+            return intervals;
+
+        Arrays.sort(intervals, Comparator.comparingInt((int[] a) -> a[0]));
+
+        List<int[]> result = new ArrayList<>();
+        for (int[] interval : intervals) {
+            if (result.size() == 0 || result.get(result.size() - 1)[1] < interval[0])
+                result.add(interval);
+            else
+                result.get(result.size() - 1)[1] =
+                    Math.max(result.get(result.size() - 1)[1], interval[1]);
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+}
